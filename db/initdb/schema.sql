629b787dcc6a785987daee4ae667d445316d51dd
tebukuro <58774766+tebukurokun@users.noreply.github.com>
2022-01-22 14:42:42 +0900

    add career mutation

--- db/initdb/schema.sql
+++ db/initdb/schema.sql
@@ -9,5 +9,7 @@ create table career (
   id serial primary key,
   name varchar(255) not null,
   person_id int not null,
+  start_year int not null,
+  end_year int not null,
   created_at timestamp not null
 );
--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,32 @@ struct Person {
     id: i32,
     name: String,
     age: i32,
+    career: Career,
+    created_at: String,
+}
+
+#[derive(SimpleObject)]
+struct Career {
+    id: i32,
+    person_id: i32,
+    name: String,
+    start_year: i32,
+    end_year: i32,
+    created_at: String,
 }
 
 #[derive(InputObject)]
 struct CreatePersonInput {
     name: String,
     age: i32,
+    career: CreateCareerInput,
+}
+
+#[derive(InputObject)]
+struct CreateCareerInput {
+    name: String,
+    start_year: i32,
+    end_year: i32,
 }
 
 #[derive(Debug, FromRow)]
@@ -39,6 +59,16 @@ struct PersonRecord {
     id: i32,
     name: String,
     age: i32,
+    created_at: NaiveDateTime,
+}
+
+#[derive(Debug, FromRow)]
+struct CareerRecord {
+    id: i32,
+    person_id: i32,
+    name: String,
+    start_year: i32,
+    end_year: i32,
     created_at: NaiveDateTime,
 }
 
@@ -53,6 +83,7 @@ impl MutationRoot {
     ) -> Result<Person, async_graphql::Error> {
         let pool = ctx.data::<PgPool>()?;
         let mut tx = pool.begin().await?;
+
         let sql = "
             insert into person (
                 name, age, created_at
@@ -66,15 +97,47 @@ impl MutationRoot {
         ";
 
         let person_record: PersonRecord = sqlx::query_as(sql)
-            .bind(input.name)
-            .bind(input.age)
+            .bind(&input.name)
+            .bind(&input.age)
             .fetch_one(&mut tx)
             .await?;
+
+        let sql = "
+            insert into career (
+                person_id, name, start_year, end_year, created_at
+            ) 
+            values 
+            (
+                $1, $2, $3, $4, current_timestamp
+            )
+            returning id, person_id, name, start_year, end_year, created_at 
+            ;
+        ";
+
+        let career_record: CareerRecord = sqlx::query_as(sql)
+            .bind(&person_record.id)
+            .bind(&input.career.name)
+            .bind(&input.career.start_year)
+            .bind(&input.career.end_year)
+            .fetch_one(&mut tx)
+            .await?;
+
+        dbg!(&person_record);
+        dbg!(&career_record);
 
         let gql_person = Person {
             id: person_record.id,
             name: person_record.name,
             age: person_record.age,
+            career: Career {
+                id: career_record.id,
+                person_id: career_record.person_id,
+                name: career_record.name,
+                start_year: career_record.start_year,
+                end_year: career_record.end_year,
+                created_at: career_record.created_at.to_string(),
+            },
+            created_at: person_record.created_at.to_string(),
         };
 
         tx.commit().await?;
@@ -106,7 +169,7 @@ async fn rocket() -> _ {
         .max_connections(5)
         .connect("postgres://user:password@localhost:54321/person_db")
         .await
-        .unwrap();
+        .expect("Failed to connect to database");
     let schema = Schema::build(QueryRoot, MutationRoot, EmptySubscription)
         .data(pool)
         .finish();
